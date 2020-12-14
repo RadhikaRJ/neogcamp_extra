96cad33ced69ce6fdb132c56621d4bcc71d0b0d2
RadhikaRJ <radhika7002@gmail.com>
2020-12-14 12:16:57 +0530

    feat: for_loop_print_nameWithNumber

---
+++ lesson_1/for_loop_number_with_name.js
@@ -0,0 +1,11 @@
+//program to print number with name using for loop
+for(var i=0; i<5;i++){
+    console.log(i,"Radhika")
+}
+//Output:
+// E:\Documents\GitHub\neogcamp_extra> node E:\Documents\GitHub\neogcamp_extra\lesson_1\for_loop_number_with_name.js
+// 0 Radhika
+// 1 Radhika
+// 2 Radhika
+// 3 Radhika
+// 4 Radhika
