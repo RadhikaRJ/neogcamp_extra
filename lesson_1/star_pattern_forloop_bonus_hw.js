1d77f18c283def1bab982d881d2af37f249edb79
RadhikaRJ <radhika7002@gmail.com>
2020-12-14 15:08:06 +0530

    Reverse print  stars in desc order

--- lesson_1/star_pattern_forloop_bonus_hw.js
+++ lesson_1/star_pattern_forloop_bonus_hw.js
@@ -24,3 +24,33 @@ for (var i = 0; i < num; i++) {
 // **********
 
 // E:\Documents\GitHub\neogcamp_extra>
+
+console.log("Reverse pattern:")
+var revnum=readlineSync.question("Enter the number of rows: ")
+
+var op1 = "";
+for (var i = revnum; i >=0; i--) {
+    for(var j=i;j>0;j--){
+        op1 = op1 + "*";
+    }
+    
+    console.log(op1);
+    op1="";
+
+}
+
+//Output:
+// E:\Documents\GitHub\neogcamp_extra>node E:\Documents\GitHub\neogcamp_extra\lesson_1\star_pattern_forloop_bonus_hw.js
+// Enter the number of rows: 5
+// *
+// **
+// ***
+// ****
+// *****
+// Reverse pattern:
+// Enter the number of rows: 5
+// *****
+// ****
+// ***
+// **
+// *
