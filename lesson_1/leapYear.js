31f3dcbb93932e97b5442a6f0ff9ce728f5a9ae6
RadhikaRJ <radhika7002@gmail.com>
2020-12-14 18:51:35 +0530

    feat: Chalk package implemented

--- lesson_1/leapYear.js
+++ lesson_1/leapYear.js
@@ -1,10 +1,15 @@
 var readlineSync = require('readline-sync');
-var year = readlineSync.question("Welcome! Enter your year of birth to identify if you were born in a leap year or not.\n Year: ");
+var chalk =require('chalk')
+var username = readlineSync.question(chalk.white.bgBlue.bold("Welcome! \n Please enter your name to proceed towards checking if you are born in a leap year \n"));
+var year = readlineSync.question(chalk.white.bgBlue.bold("Thanks "+username+" Enter your birth year to check if it was a leap year or not.\n Year: "));
 
 function leapYearCalc(year){
   if((year% 4 == 0 && year%100!=0)||( year% 400 == 0)){
-    console.log("Yes, "+ year+" is a leap year !");
+    console.log(chalk.blue.bgWhite.bold(username+ ", your birth year: "+year+" is a leap year.\n Feel free to share this to your friend circle over social media"));
   } 
-  else(console.log("You were not born in a leap year!"));
+  else{
+    console.log(chalk.red.bgWhite.bold(username+", you were not born in a leap year!"));
+  }
 }
+
 leapYearCalc(year);
