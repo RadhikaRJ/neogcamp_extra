4f37db9d16f683aab250e344d5295d62d1a28c3b
RadhikaRJ <radhika7002@gmail.com>
2020-12-17 11:19:44 +0530

    feat: prime number check

--- lesson_1/prime_number.js
+++ lesson_1/prime_number.js
@@ -6,3 +6,22 @@ var username = readlineSync.question(chalk.white.bgBlue.bold("Welcome! \n Please
 
 console.log(chalk.white.bgBlue.bold("\n Enter the date in following format:\n DD/MM \n 25/12\n"))
 dob=readlineSync.question(chalk.white.bgBlue.bold("\nEnter your date of birth in above mentioned format: "))
+
+function primeNumberChecker(finaldate){
+    var birthdate=Number(finaldate);
+    var flag=0;
+    
+    for(var i=1;i<finaldate;i++){
+      if(birthdate%i==0){
+        flag++;
+      }
+      if(flag>1){
+        break;
+      }
+    }
+    flag>1
+    ?console.log(chalk.red.bgWhite.bold("\n"+username+", your birthdate is not prime!"))
+    :console.log(chalk.blue.bgWhite.bold("\n"+username+ ", your birthdate is prime.\n Feel free to share this with your friend circle over social media"));
+    
+  }
+  primeNumberChecker(dob);
