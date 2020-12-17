c0566f61222961adcbc405598d9dde0332a0bf04
RadhikaRJ <radhika7002@gmail.com>
2020-12-17 12:14:03 +0530

    style: updated color to Magenta

--- lesson_1/prime_number.js
+++ lesson_1/prime_number.js
@@ -1,11 +1,11 @@
 var readlineSync = require('readline-sync');
 var chalk =require('chalk')
-var username = readlineSync.question(chalk.white.bgBlue.bold("Welcome! \n Please enter your name to proceed towards checking if your birthdate is a prime number \n"));
+var username = readlineSync.question(chalk.white.bgMagenta.bold("Welcome! \n Please enter your name to proceed towards checking if your birthdate is a prime number \n"));
 
 
 
-console.log(chalk.white.bgBlue.bold("\n Enter the date in following format:\n DD/MM \n 25/12\n"))
-dob=readlineSync.question(chalk.white.bgBlue.bold("\nEnter your date of birth in above mentioned format: "))
+console.log(chalk.white.bgMagenta.bold("\n Enter the date in following format:\n DD/MM \n 25/12\n"))
+dob=readlineSync.question(chalk.white.bgMagenta.bold("\nEnter your date of birth in above mentioned format: "))
 
 
 function checkDateInput(date)
@@ -26,12 +26,12 @@ function checkDateInput(date)
   isNaN(monthcheck)== true||
   monthcheck==02 && daycheck==30)
   {
-    console.log("daycheck: "+daycheck+"monthcheck: "+monthcheck);
-     console.log("Number(daycheck): "+Number(daycheck)+"Number(monthcheck): "+Number(monthcheck));
+    // console.log("daycheck: "+daycheck+"monthcheck: "+monthcheck);
+    //  console.log("Number(daycheck): "+Number(daycheck)+"Number(monthcheck): "+Number(monthcheck));
   return false;
   }
   else {
-    console.log("Number(daycheck): "+Number(daycheck)+"Number(monthcheck): "+Number(monthcheck));
+    // console.log("Number(daycheck): "+Number(daycheck)+"Number(monthcheck): "+Number(monthcheck));
     return true;}
 }
 
@@ -64,7 +64,7 @@ function primeNumberChecker(finaldate){
   }
   flag>1
   ?console.log(chalk.red.bgWhite.bold("\n"+username+", your birthdate is not prime!"))
-  :console.log(chalk.blue.bgWhite.bold("\n"+username+ ", your birthdate is prime.\n Feel free to share this with your friend circle over social media"));
+  :console.log(chalk.magenta.bgWhite.bold("\n"+username+ ", your birthdate is prime.\n Feel free to share this with your friend circle over social media"));
   
 }
 
